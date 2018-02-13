0277e6c51c664e57429632281cb608d9a20582c5
DABAYO <dbayo2000@yahoo.com>
2018-02-12 20:29:57 -0500

    begin

--- liri.js
+++ liri.js
@@ -1,30 +1,52 @@
+//set global info
 require("dotenv").config();
-
-// nodes to use
 const spotifyNode = require("node-spotify-api");
 const twitterNode = require("twitter");
 const request = require("request")
 const fs = require("fs");
-
+const arg2 = process.argv[2];
+const arg3 = process.argv[3];
 //keys required
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
-
-//my-tweet code below displays 10 most recent tweets
-const song = process.argv[2];
-const movieName = process.argv[3];
-const omdbApi = 96675a9;
+const omdbApi = "96675a9";
 const queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=' + omdbApi;
 
 
-// spotify-this-song takes user input and displays song info
+switch (action) {
+  case "my-tweets":
+    myTweets();
+    break;
 
-// movie-this '<movie name here>'
+  case "spotify-this-song":
+    spotifySong();
+    break;
 
+  case "movie-this":
+    movie();
+    break;
 
-//do-what-it-says reads random.txt file and executes
-fs.readFile("random.txt", "utf8", function(err, data) {
+  case "do-what-it-says":
+    comply();
+    break;
+}
+
+function myTweets(){
+
+};
+
+function spotifySong() {
+
+};
+
+function movie(){
+
+};
+
+function comply() {
+  fs.readFile("random.txt", "utf8", function(err, data) {
     if (err) {
       return console.log(err);
     }
-}
+});
+
