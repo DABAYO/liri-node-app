68551d942030ff4c6e46fb8d9df2162f88c64b29
DABAYO <dbayo2000@yahoo.com>
2018-02-12 21:06:22 -0500

    setup

--- liri.js
+++ liri.js
@@ -15,32 +15,53 @@ const queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apik
 
 switch (action) {
   case "my-tweets":
-    myTweets();
+    myTweets(arg1);
     break;
 
   case "spotify-this-song":
-    spotifySong();
+    spotifySong(arg3);
     break;
 
   case "movie-this":
-    movie();
+    movie(arg3);
     break;
 
   case "do-what-it-says":
-    comply();
+    comply(arg2);
     break;
 }
 
 function myTweets(){
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
 
 };
 
 function spotifySong() {
-
+  spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+     // Do something with 'data' 
+});
 };
 
 function movie(){
+  request(queryUrl, function(error, response, body) {
 
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+  
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      console.log('Release Year: ' + JSON.parse(body).Year);
+    }
+  });
 };
 
 function comply() {
