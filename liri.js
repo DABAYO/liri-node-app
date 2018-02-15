3397848633ca1a00745832fe6fc065055fc13a63
DABAYO <dbayo2000@yahoo.com>
2018-02-15 18:22:04 -0500

    Thursday

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-//set global info
+/set global info
 require("dotenv").config();
 const spotifyNode = require("node-spotify-api");
 const twitterNode = require("twitter");
@@ -13,58 +13,91 @@ const omdbApi = "96675a9";
 const queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=' + omdbApi;
 // end of global info
 
-switch (action) {
+// list of different options for argument2
+switch (arg2) {
   case "my-tweets":
-    myTweets(arg2);
+    myTweets();
     break;
 
   case "spotify-this-song":
-    spotifySong(arg3);
+    spotifySong();
     break;
 
   case "movie-this":
-    movie(arg3);
+    movie();
     break;
 
   case "do-what-it-says":
-    comply(arg2);
+    doWhatItSays();
     break;
 }
 
-function myTweets(){
-  var params = arg2;
+// console log my 20 latest tweets
+function myTweets() {
+  var params = {screen_name: 'chirpy_derby',count:20};
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
-
-function spotifySong() {
-  spotify.search({arg3}, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-     // Do something with 'data' 
-});
-
-function movie(){
-  request(queryUrl, function(error, response, body) {
-
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
-  
-      // Parse the body of the site and recover just the imdbRating
-      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      console.log('Release Year: ' + JSON.parse(body).Year);
+    if (!error) {
+      console.log("chirpy_derby's latest tweets:");
+      for (let i = 0; i < tweets.length; i++){
+        console.log(JSON.stringify(tweets[i].text, null, 2));
+      }
     }
   });
-};
+}
 
-function comply() {
-  fs.readFile("random.txt", "utf8", function(err, data) {
+// look up a song
+function spotifySong() {
+  if (!arg3) {
+    console.log("Can't decide? How about this?")
+    arg3 = "The Sign";
+  };
+  spotify.search({ type:'track', query: arg3, market:"us", limit:1 }, function(err, data) {
     if (err) {
-      return console.log(err);
+      return console.log('Error occurred: ' + err);
     }
-});
+    console.log(JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+    console.log(JSON.stringify(data.tracks.items[0].name, null, 2));
+    console.log(JSON.stringify(data.tracks.items[0].external_urls.spotify, null, 2));
+    console.log(JSON.stringify(data.tracks.items[0].album.name, null, 2));
+  });
+}
 
+//Look up a movie title
+function movie() {
+  if (!arg3) {
+    arg3 = "Mr.Nobody";
+  }
+    request(queryUrl, function (err, response, body) {
+        console.log(JSON.parse(body).Ratings.length);
+        if (!err && response.statusCode === 200) {
+              console.log(`${arg3} info:
+              year: ${JSON.parse(body).Year}
+              IMDB Rating: ${JSON.parse(body).imdbRating} 
+              Country where produced: ${JSON.parse(body).Country} 
+              Language: ${JSON.parse(body).Language} 
+              Plot: ${JSON.parse(body).Plot} 
+              Actors: ${JSON.parse(body).Actors}  `);
+
+              let rotten = JSON.parse(body).Ratings;
+              for (let i = 0; i < rotten.length; i++){
+                  if (rotten[i].Source === "Rotten Tomatoes"){
+                    console.log(`Rotten Tomatoes Rating: ${rotten[i].Value} `);
+                  }
+              }    
+        }
+    });
+}
+
+// read random.txt
+function doWhatItSays() {
+
+    fs.readFile('random.txt','utf8',  function(error, data) {
+      if (error) {
+        return console.log(error);
+      }
+      var dataArr = data.split(",");
+  
+      arg2 = dataArr[0].toString();
+      arg3 = dataArr[1].toString();
+    });
+}
